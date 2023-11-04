f408a59a97e94795b5594376cc3c43833594f41d
喻小伟 <still.vv@qq.com>
2023-11-04 22:31:29 +0800

    feat: 添加验证

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/health', (req, res) => {
   res.send('ok!')
 })
 
+app.get('WW_verify_LaNOHYT5S8N4cntA.txt', (req, res) => {
+  res.send('LaNOHYT5S8N4cntA')
+})
+
 app.use('/', proxy('https://qyapi.weixin.qq.com'));
 
 app.listen(port, () => {
